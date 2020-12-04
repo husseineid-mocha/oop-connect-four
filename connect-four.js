58ab58e795fd58b8ad091273f4735f269732af2d
tanbre <tannor@breitigam.email>
2020-12-04 14:47:26 -0800

    connect four 2

--- connect-four.js
+++ connect-four.js
@@ -2,6 +2,17 @@ import Game from './game.js'
 
 let game = undefined
 
+const updateUI = function() {
+  const boardHolder = document.getElementById('board-holder')
+  const gameName = document.getElementById('game-name')
+
+  if (!game) boardHolder.classList.add('is-invisible')
+  else {
+    boardHolder.classList.remove('is-invisible')
+    gameName.innerHTML = game.getName()
+  }
+}
+
 window.addEventListener("DOMContentLoaded", event => {
     const nameOneInput = document.getElementById("player-1-name")
     const nameTwoInput = document.getElementById("player-2-name")
@@ -9,16 +20,25 @@ window.addEventListener("DOMContentLoaded", event => {
     const newGameBtn = document.getElementById("new-game")
 
     formHolder.addEventListener("keyup", event => {
-        nameOneValue = nameOneInput.value
-        newTwoValue = nameTwoInput.value
+        let nameOneValue = nameOneInput.value
+        let nameTwoValue = nameTwoInput.value
+        console.log(`file: connect-four.js ~ line 32 ~ nameTwoValue`, nameTwoValue);
+        console.log(`file: connect-four.js ~ line 32 ~ nameOneValue`, nameOneValue);
+        console.log(`file: connect-four.js ~ line 29 ~ nameOneValue && nameTwoValue`, nameOneValue.length > 0 && nameTwoValue.length > 0);
 
-        if (nameOneValue && nameTwoValue) {
-            newGameBtn.setAttribute("disabled", false)
+        if (nameOneValue.length > 0 && nameTwoValue.length > 0) {
+            newGameBtn.attributes.removeNamedItem('disabled')
         } else {
-            newGameBtn.setAttribute("disabled", true)
+            newGameBtn.setAttribute('disabled', '')
         }
     })
 
-
+    newGameBtn.addEventListener('click', event => {
+      game = new Game(nameOneInput.value, nameTwoInput.value)
+      nameOneInput.value = ''
+      nameTwoInput.value = ''
+      newGameBtn.setAttribute("disabled", "")
+      updateUI()
+    })
 
 })
